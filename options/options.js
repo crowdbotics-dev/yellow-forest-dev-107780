93ab7eb686eb290fbc8d05630868b7f6e7a6fd4c
Crowdbotics <engineering@crowdbotics.com>
2023-08-07 07:53:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-forest-dev-107780",
+  url: "https://yellow-forest-dev-107780.botics.co",
+  api: "https://yellow-forest-dev-107780.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
